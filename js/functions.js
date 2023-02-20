a356f9deeb6bb3048ca6fc746a5a7c6b1dc1c4b8
Vylich <ilya.sidorenko.99@gmail.com>
2023-02-20 16:33:10 +0300

    Рефракторинг кода

--- js/functions.js
+++ js/functions.js
@@ -1,28 +1,18 @@
 // Функция для проверки длины строки
-function formValidation(string, n) {
-  if (string.length === n) {
-    return true;
-  }else {
-    return false;
-  }
-}
+const formValidation = (string, n) => string.length === n;
 formValidation();
 
 // Функция для проверки, является ли строка палиндромом.
-function checkPalindrome(string) {
+const checkPalindrome = (string) => {
   const normalStr = string.toLowerCase().split(' ').join('');
   const reverseStr = normalStr.split('').reverse().join('');
-  if (normalStr === reverseStr) {
-    return true;
-  }else {
-    return false;
-  }
-}
+  return normalStr === reverseStr;
+};
 checkPalindrome();
 
 // Функция, которая принимает строку, извлекает содержащиеся в
 // ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
-function returnNumber(string) {
+const returnNumber = (string) => {
   const numberToString = String(string).split('').join('');
   let result = '';
   for (let i = 0; i < numberToString.length; i++) {
@@ -31,24 +21,23 @@ function returnNumber(string) {
     }
   }
   return result;
-}
+};
 returnNumber();
 
 // Функция, которая принимает три параметра: исходную строку,
 // минимальную длину и строку с добавочными символами — и возвращает исходную строку,
 // дополненную указанными символами до заданной длины.
-function generationAddress(strStart, minLen, strPad) {
+const generationAddress = (strStart, minLen, strPad) => {
   let res = '';
   const diffLen = minLen - strStart.length;
   if (strStart.length >= minLen) {
     return strStart;
   }else if (strStart.length < minLen) {
-    res = strPad.substr(0, diffLen) + strStart;
+    res = strPad.substring(0, diffLen) + strStart;
     while (res.length < minLen) {
-      res = strPad.substr(0, minLen - res.length) + res;
+      res = strPad.substring(0, minLen - res.length) + res;
     }
     return res;
   }
-}
+};
 generationAddress();
-
