b71da4fd6d29721cdf73ee865aa77f16de27255a
Vylich <ilya.sidorenko.99@gmail.com>
2023-02-26 20:28:43 +0300

    разделение на модули

---
+++ js/modules/posts.js
@@ -0,0 +1,20 @@
+import {addDescription, getRandomInteger} from './util.js';
+import {POST_MIN_LIKES, POST_MAX_LIKES, COMMENT_MAX_COUNT, COMMENT_MIN_COUNT} from './variables.js';
+import {addComment} from './comments.js';
+
+
+const addPost = (_, index) => {
+  const id = index + 1;
+  return {
+    id,
+    url: `photos/${id}.jpg`,
+    description: addDescription(),
+    likes: getRandomInteger(POST_MIN_LIKES, POST_MAX_LIKES),
+    comments: Array.from({ length: getRandomInteger(COMMENT_MIN_COUNT, COMMENT_MAX_COUNT) }, addComment),
+  };
+};
+
+const posts = () => Array.from({ length: 25 }, addPost);
+
+
+export {addPost, posts};
---
+++ js/modules/util.js
@@ -0,0 +1,45 @@
+import {COMMENT_ID_START, COMMENT_ID_STEP, TEXT_DESCRIPTION} from './variables.js';
+
+const getRandomInteger = (min, max) => {
+  if ((typeof min !== 'number' && typeof max !== 'number') || (min < 0 && max < 0)) {
+    return null;
+  }
+  if (min < 0) {
+    min = 0;
+  }
+  if (max < 0) {
+    max = 0;
+  }
+  if (min === max) {
+    return Math.floor(min);
+  }
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(min + Math.random() * (max + 1 - min));
+};
+
+const getArrRandomElem = (arr) => arr[getRandomInteger(0, arr.length - 1)];
+
+const shuffleArr = (arr) => [...arr].sort(() => Math.random() - 0.5);
+
+const generateId = (start = 0, maxStep = 1) => {
+  let id = start;
+  const minStep = 1;
+  return () => {
+    id += getRandomInteger(minStep, maxStep);
+    return id;
+  };
+};
+
+const getCommentNextId = () => generateId(COMMENT_ID_START, COMMENT_ID_STEP);
+
+const addDescription = () => {
+  const descriptionCount = 1;
+  return shuffleArr(TEXT_DESCRIPTION).slice(0, descriptionCount).join(' ');
+};
+
+
+export {getArrRandomElem, shuffleArr, generateId, getRandomInteger, getCommentNextId, addDescription};
