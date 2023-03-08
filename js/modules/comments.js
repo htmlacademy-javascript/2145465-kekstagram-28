9a049573483f5dd1fffa82ec90603665a0f2b211
Vylich <ilya.sidorenko.99@gmail.com>
2023-03-08 20:58:04 +0300

    Отрисовка комментариев по 5 штук

--- js/modules/comments.js
+++ js/modules/comments.js
@@ -1,6 +1,5 @@
 import {TEXT_MESSAGES, USER_AVATAR_MAX_ID, USER_AVATAR_MIN_ID, COMMENT_AUTHOR_NAMES} from './variables.js';
 import {getRandomInteger, getCommentNextId, shuffleArr, getArrRandomElem} from './util.js';
-import { socialCommentsNode, socialOneCommentNode } from './rendering-full-photos.js';
 
 const addCommentMessage = () => {
   const commentMinCount = 1;
@@ -16,19 +15,4 @@ const addComment = () => ({
   name: getArrRandomElem(COMMENT_AUTHOR_NAMES)
 });
 
-function createFullComment(userComments) {
-  const commentFragment = document.createDocumentFragment();
-
-  userComments.comments.forEach((userComment) => {
-    const commentItem = socialOneCommentNode.cloneNode(true);
-
-    commentItem.querySelector('.social__picture').src = userComment.avatar;
-    commentItem.querySelector('.social__text').textContent = userComment.message;
-    commentFragment.append(commentItem);
-  });
-
-  socialCommentsNode.innerHTML = '';
-  socialCommentsNode.append(commentFragment);
-}
-
-export {addCommentMessage, addComment, createFullComment};
+export {addCommentMessage, addComment};
